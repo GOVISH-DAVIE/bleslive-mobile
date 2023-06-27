9709b042619751883c551cee191ca8514dbb9dd4
Govish-Davie <9davidmuia@gmail.com>
2023-06-27 16:00:23 +0300

    login

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,6 +1,9 @@
+import 'package:bleslive/screens/dashboard/dashboard.dart';
 import 'package:bleslive/screens/login/login.dart';
 import 'package:flutter/widgets.dart';
 
 final Map<String, WidgetBuilder> routes = {
   Login.route: (context) => const Login(),
+  Dashboard.route: (context) => const Dashboard(),
+
 };
